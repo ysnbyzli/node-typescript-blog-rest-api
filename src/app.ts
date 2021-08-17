8e905b4d6cc41251788773252552954ba0d8d0e3
ysnbyzli <yasinbeyazli29@gmail.com>
2021-08-17 21:32:07 +0300

    add packet logger and morgan

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,32 @@
 import express from 'express';
 import * as dotenv from 'dotenv';
+import Logger from './utils/logger'
+import loggerMiddleware from './middlewares/logger.middleware';
+
 
 const app = express();
 dotenv.config();
 
+// middlewares
+app.use(loggerMiddleware)
+
+
+
 if (!process.env.PORT) {
-  process.exit(0);
+    process.exit(0);
 }
 
+
+app.get('/logger', (_, res) => {
+    Logger.error('This is an error log');
+    Logger.warn('This is a warn log');
+    Logger.info('This is a info log');
+    Logger.debug('This is a debug log');
+
+    res.send('Hello world');
+})
+
+
 app.listen(process.env.PORT, () => {
-  console.log(`server is started running on port ${process.env.PORT} 🚀🚀`);
+    console.log(`server is started running on port ${process.env.PORT} 🚀🚀`);
 });
