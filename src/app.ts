10164851d52b3f725834abdfc54a043112f89342
ysnbyzli <yasinbeyazli29@gmail.com>
2021-08-17 21:19:36 +0300

    created eslint and prettier

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,10 @@ import * as dotenv from 'dotenv';
 const app = express();
 dotenv.config();
 
-
-if(!process.env.PORT){
-    process.exit(0);
+if (!process.env.PORT) {
+  process.exit(0);
 }
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is started running on port ${process.env.PORT} 🚀🚀`)
-})
+  console.log(`server is started running on port ${process.env.PORT} 🚀🚀`);
+});
